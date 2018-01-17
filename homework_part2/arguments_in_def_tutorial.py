4579ba3bac0f8390af55a4de0045451beb8d6da7
Alena Subbotina <a.subbotina@plamee.com>
2018-01-17 16:23:59 +0300

    add arguments add 2

--- homework_part2/arguments_in_def_tutorial.py
+++ homework_part2/arguments_in_def_tutorial.py
@@ -4,13 +4,17 @@ def printab(a, b):
 
 # CORRECT WAYS TO CALL A FUNCTION
 printab(10, 20)
-printab(a = 10, b = 20)
+printab(a=10, b=20)
 
 # reyword arguments always after non-keyword arguments
-printab(10, b = 20)
+printab(10, b=20)
 
 lst = [10, 20]
 printab(*lst) # = printab(lst[0], lst[1])
 
 args = {'a':10, 'b': 20}
 printab(**args) # = printab(key1 = args[key1], key2 = args[key2])
+
+def printab(a = 10, b = 10): # аргументы по умолчанию (должны быть полсе обычных)
+    print(a)
+    print(b)
