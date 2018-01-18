3ba3c16ec72181631218b4a8f5315e44b2ed4605
Alena Subbotina <a.subbotina@plamee.com>
2018-01-18 17:13:01 +0300

    add construction_hw

---
+++ homework_part2/construction_hw.py
@@ -0,0 +1,35 @@
+n = int(input())
+var = ({'global': set()})
+parent = ({'global': None})
+lst = []
+
+def create(dict1, dict2, i, j):
+    dict1[i] = set()
+    dict2[i] = j
+    return dict1, dict2
+
+def add(dict, i, j):
+    if i in dict:
+        dict[i].add(j)
+    return dict
+
+def get(dict1, dict2, i, j):
+    if j in dict1[i]:
+        return i
+    elif i == 'global':
+        return None
+    else:
+        i = dict2[i]
+        return get(dict1, dict2, i, j)
+
+for i in range(n):
+    cmd, namesp, arg = input().split()
+    if cmd == 'add':
+        add(var, namesp, arg)
+    elif cmd == 'create':
+        create(var, parent, namesp, arg)
+    elif cmd == 'get':
+        get(var, parent, namesp, arg)
+        lst.append(get(var, parent, namesp, arg))
+for i in range(len(lst)):
+    print(lst[i])
