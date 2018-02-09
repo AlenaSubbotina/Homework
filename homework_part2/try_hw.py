0e0cf5793209a8f89e91465eff02fe135e5b6667
Alena Subbotina <a.subbotina@plamee.com>
2018-02-09 17:39:39 +0300

    add try_except hw

---
+++ homework_part2/try_hw.py
@@ -0,0 +1,10 @@
+
+try:
+    foo()
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+except ArithmeticError:
+    print("ArithmeticError")
+except AssertionError:
+    print("AssertionError")
+
