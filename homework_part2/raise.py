8c08de2b474ff651e0693b9d8a884c34c62de31a
Alena Subbotina <a.subbotina@plamee.com>
2018-02-10 12:27:59 +0300

    add raise

---
+++ homework_part2/raise.py
@@ -0,0 +1,29 @@
+
+def greet(name):
+    if name[0].isupper():
+        return "Hello, " + name
+    else:
+        raise ValueError(name + " is inappropriate name")
+
+while True:
+    try:
+        name = input("Please enter your name: ")
+        greeting = greet(name)
+        print(greeting)
+    except ValueError:
+        print("Please try again")
+    else:
+        break
+
+# можно написать класс
+#class BadName(Exception):
+#    pass
+
+#def greet(name):
+#    if name[0].isupper():
+#        return "Hello, " + name
+#    else:
+#        raise BadName(name + " is inappropriate name")
+
+#print(greet("Anton"))
+#print(greet("anton"))
